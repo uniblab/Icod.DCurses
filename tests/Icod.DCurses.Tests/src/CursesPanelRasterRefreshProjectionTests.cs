26afbf7cea66109d885a46d7c1e348910b453c78
Timothy Bruce <uniblab@hotmail.com>
2026-09-15 18:33:14 -0400

    test: add T1606 panel raster projection RED

---
+++ tests/Icod.DCurses.Tests/src/CursesPanelRasterRefreshProjectionTests.cs
@@ -0,0 +1,118 @@
+/*
+	Icod.DCurses.Tests
+	Automated test suite for Icod.DCurses.
+	Copyright (C) 2026  Timothy J. Bruce <uniblab@hotmail.com>
+*/
+
+/*
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Xunit;
+
+namespace Icod.DCurses.Tests;
+
+/// <summary>Drives the T1606 panel-composition to physical-refresh raster projection seam.</summary>
+public sealed class CursesPanelRasterRefreshProjectionTests {
+	[Fact]
+	public void InitialPanelRefreshProjectionCarriesRasterAxis() {
+		CursesSession session = CreateProjectionOnlySession();
+		CursesScreen source = new( 3, 1 );
+		CursesVirtualScreen composed = new( 3, 1 );
+		CursesRasterCell token = CursesRasterRepresentationBaselineTests.CreateLogicalRasterCell();
+		composed.SetRasterCell( 0, 1, token );
+
+		CursesScreen projection = SynchronizeProjection(
+			session,
+			source,
+			composed
+		);
+
+		Assert.Equal( token, projection.VirtualScreen.GetRasterCell( 0, 1 ) );
+	}
+
+	[Fact]
+	public void IncrementalPanelRefreshProjectionAddsRasterWithoutChangingVisualCell() {
+		CursesSession session = CreateProjectionOnlySession();
+		CursesScreen source = new( 3, 1 );
+		CursesVirtualScreen composed = new( 3, 1 );
+		_ = SynchronizeProjection(
+			session,
+			source,
+			composed
+		);
+		composed.MarkClean();
+		CursesRasterCell token = CursesRasterRepresentationBaselineTests.CreateLogicalRasterCell();
+		composed.SetRasterCell( 0, 2, token );
+
+		CursesScreen projection = SynchronizeProjection(
+			session,
+			source,
+			composed
+		);
+
+		Assert.Equal( token, projection.VirtualScreen.GetRasterCell( 0, 2 ) );
+	}
+
+	[Fact]
+	public void IncrementalPanelRefreshProjectionRemovesStaleRaster() {
+		CursesSession session = CreateProjectionOnlySession();
+		CursesScreen source = new( 3, 1 );
+		CursesVirtualScreen composed = new( 3, 1 );
+		CursesScreen projection = SynchronizeProjection(
+			session,
+			source,
+			composed
+		);
+		CursesRasterCell token = CursesRasterRepresentationBaselineTests.CreateLogicalRasterCell();
+		projection.VirtualScreen.SetRasterCell( 0, 1, token );
+		projection.VirtualScreen.MarkClean();
+		composed.MarkClean();
+		composed.TouchCell( 0, 1 );
+
+		projection = SynchronizeProjection(
+			session,
+			source,
+			composed
+		);
+
+		Assert.Null( projection.VirtualScreen.GetRasterCell( 0, 1 ) );
+	}
+
+	private static CursesSession CreateProjectionOnlySession() {
+		return (CursesSession)RuntimeHelpers.GetUninitializedObject(
+			typeof( CursesSession )
+		);
+	}
+
+	private static CursesScreen SynchronizeProjection(
+		CursesSession session,
+		CursesScreen source,
+		CursesVirtualScreen composed
+	) {
+		MethodInfo method = Assert.IsAssignableFrom<MethodInfo>(
+			typeof( CursesSession ).GetMethod(
+				"SynchronizePanelRefreshProjection",
+				BindingFlags.Instance | BindingFlags.NonPublic
+			)
+		);
+		object? result = method.Invoke(
+			session,
+			[ source, composed ]
+		);
+		return Assert.IsType<CursesScreen>( result );
+	}
+}
